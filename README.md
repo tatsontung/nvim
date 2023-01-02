be693b57187ee60c624fbad1e2d3dd8cce7eaec1
Tat Son Tung NGUYEN <tatsontung.nguyen@gmail.com>
2023-01-02 01:23:51 +0100

    updated readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# A Basic Stable IDE config for Neovim
+
+Highly inspired by <https://github.com/LunarVim/nvim-basic-ide>
--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -22,7 +22,7 @@ dashboard.section.buttons.val = {
   dashboard.button("q", " " .. " Quit", ":qa<CR>"),
 }
 local function footer()
-  return "chrisatmachine.com"
+  return "All thanks to https://github.com/LunarVim/nvim-basic-ide"
 end
 
 dashboard.section.footer.val = footer()
